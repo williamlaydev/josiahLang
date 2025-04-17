79e95e9e76e5710f82edfd1efc422afc1082519d
William Lay <>
2025-04-17 15:34:38 +0900

    Create string functions for AST node

--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -75,6 +77,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: Skip expressions until we encounter a semicolon
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return statement
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	statement := &ast.ReturnStatement{Token: p.currToken}
+
+	p.nextToken()
 
 	// TODO: Skip expressions until we encounter a semicolon
 	for !p.currTokenIs(token.SEMICOLON) {
