1ba441f54c28c49387d71607a8ff0b571237a765
William Lay <willlaydev@gmail.com>
2025-05-03 23:18:29 +1000

    Additional built in functions

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,18 +3,61 @@ package evaluator
 import "josiahLang/object"
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
+	"len":  {Fn: builtInLen},
+	"last": {Fn: last},
+	"push": {Fn: push},
+}
 
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("argument to `len` not supported, got %s", args[0].Type())
-			}
-		},
-	},
+func builtInLen(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+
+	switch arg := args[0].(type) {
+	case *object.Array:
+		return &object.Integer{Value: int64(len(arg.Elements))}
+	case *object.String:
+		return &object.Integer{Value: int64(len(arg.Value))}
+	default:
+		return newError("argument to `len` not supported, got %s", args[0].Type())
+	}
+}
+
+func last(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `last` must be an ARRAY, got %s", args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Elements)
+
+	if length > 0 {
+		return arr.Elements[length-1]
+	}
+
+	return NULL
+}
+
+func push(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return newError("wrong number of arguments. got=%d, want=2", len(args))
+	}
+
+	if args[0].Type() != object.ARRAY_OBJ {
+		return newError("argument to `push` must be ARRAY, got %s",
+			args[0].Type())
+	}
+
+	arr := args[0].(*object.Array)
+	length := len(arr.Elements)
+
+	newElements := make([]object.Object, length+1)
+	copy(newElements, arr.Elements)
+	newElements[length] = args[1]
+
+	return &object.Array{Elements: newElements}
 }
