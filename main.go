9bbf3c05d79ce5d19c1df1bff90a2dbe81d183fe
William Lay <willlaydev@gmail.com>
2025-05-05 23:51:01 +1000

    Allow using source files

--- main.go
+++ main.go
@@ -1,20 +1,60 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
+	"josiahLang/evaluator"
+	"josiahLang/lexer"
+	"josiahLang/object"
+	"josiahLang/parser"
 	"josiahLang/repl"
 	"os"
-	"os/user"
 )
 
 func main() {
-	user, err := user.Current()
+	// user, err := user.Current()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Hello %s! This is the REPL for JosiahLang\n",
+	// 	user.Username)
+
+	// repl.Start(os.Stdin, os.Stdout)
+
+	sourceName := flag.String("s", "", "JosiahLang source file name")
+	flag.Parse()
+
+	// Read the file
+	file, err := os.Open(*sourceName)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error opening file:", err)
+		return
 	}
+	defer file.Close()
 
-	fmt.Printf("Hello %s! This is the REPL for JosiahLang\n",
-		user.Username)
+	scanner := bufio.NewScanner(file)
+	env := object.NewEnvironment()
 
-	repl.Start(os.Stdin, os.Stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// fmt.Println(line)
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			repl.PrintParserErrors(os.Stdout, p.Errors())
+			continue
+		}
+
+		res := evaluator.Eval(program, env)
+		if res != nil {
+			io.WriteString(os.Stdout, res.Inspect())
+			io.WriteString(os.Stdout, "\n")
+		}
+	}
 }
