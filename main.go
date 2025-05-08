9ede70f5b0be3ff03540d2a571379fb299ef7280
William Lay <willlaydev@gmail.com>
2025-05-08 19:37:37 +1000

    Error handling for code runner

--- main.go
+++ main.go
@@ -4,30 +4,25 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"josiahLang/evaluator"
 	"josiahLang/lexer"
 	"josiahLang/object"
 	"josiahLang/parser"
 	"josiahLang/repl"
 	"os"
+	"regexp"
 )
 
 func main() {
-	// user, err := user.Current()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Printf("Hello %s! This is the REPL for JosiahLang\n",
-	// 	user.Username)
-
-	// repl.Start(os.Stdin, os.Stdout)
-
 	sourceName := flag.String("s", "", "JosiahLang source file name")
 	flag.Parse()
 
 	// Read the file
+	if !isValidSourceFile(sourceName) {
+		fmt.Printf("Source wrong file type, want=*.josiah got=%s\n", *sourceName)
+		return
+	}
+
 	file, err := os.Open(*sourceName)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -35,26 +30,50 @@ func main() {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	env := object.NewEnvironment()
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		// fmt.Println(line)
-		l := lexer.New(line)
-		p := parser.New(l)
-
-		program := p.ParseProgram()
-
-		if len(p.Errors()) != 0 {
-			repl.PrintParserErrors(os.Stdout, p.Errors())
-			continue
-		}
-
-		res := evaluator.Eval(program, env)
-		if res != nil {
-			io.WriteString(os.Stdout, res.Inspect())
-			io.WriteString(os.Stdout, "\n")
-		}
+	sourceCode, err := readFile(file)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
 	}
+
+	// Source code to Go
+	env := object.NewEnvironment()
+	l := lexer.New(sourceCode)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		repl.PrintParserErrors(os.Stdout, p.Errors())
+		return
+	}
+
+	res := evaluator.Eval(program, env)
+
+	// DEBUGGING TOOL
+	if res != nil {
+		// io.WriteString(os.Stdout, res.Inspect())
+		// io.WriteString(os.Stdout, "\n")
+	}
+}
+
+func isValidSourceFile(fileName *string) bool {
+	match, _ := regexp.MatchString(`\.josiah$`, *fileName)
+	return match
+}
+
+func readFile(file *os.File) (string, error) {
+	var sourceCode string
+	scanner := bufio.NewScanner(file)
+
+	// Scan the entire file in
+	for scanner.Scan() {
+		sourceCode += scanner.Text() + "\n"
+	}
+
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("error reading file: %w", err)
+	}
+
+	return sourceCode, nil
 }
